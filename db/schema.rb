4ad51389e35c09235d28d5bbc14b1d36bb8d9634
Ваше Имя <vicstor@bk.ru>
2019-03-11 11:33:41 +0300

    lesson_end_append_2

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_10_164042) do
-
+ActiveRecord::Schema.define(version: 20_190_310_164_042) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
