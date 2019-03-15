9794f2ae66b18260c2749825e6806e779c2a80c3
Victor Guard <vicstor@bk.ru>
2019-03-15 16:17:24 +0300

    install devise and model User

--- spec/featuers/sign_in_spec.rb
+++ spec/featuers/sign_in_spec.rb
@@ -18,5 +18,11 @@ feature 'User can sign in', %q{
     expect(page).to have_content 'Signed in successfully.'
   end
 
+  scenario 'Unregistered user tries to sign in' do
+    fill_in 'Email', with: 'wrong@test.com'
+    fill_in 'Password', with: '12345678'
+    click_on 'Log in'
 
+    expect(page).to have_content 'Invalid Email or password.'
+  end
 end
