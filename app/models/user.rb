62d179b29eaa8b1c703f3cb44158cb063c1dc6fd
Victor Guard <vicstor@bk.ru>
2019-04-29 08:51:28 +0300

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :questions, foreign_key: 'author_id', dependent: :destroy
   has_many :answers, foreign_key: 'author_id', dependent: :destroy
   has_many :awards
-  has_many :votes
+  has_many :votes, dependent: :destroy
 
   def author_of?(obj)
     obj.author_id == id
