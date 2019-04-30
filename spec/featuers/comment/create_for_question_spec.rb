10023781b5c33457ca3c1dc0012b691c94daa842
Victor Guard <vicstor@bk.ru>
2019-04-30 23:01:20 +0300

    create resourse Question_comment

---
+++ spec/featuers/comment/create_for_question_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+feature 'User can create comments for question', %q{
+  In order to create comments to question
+  As an authenticated user
+  I'd like to be able to set comments for question
+} do
+
+  given(:user) { create(:user) }
+  given(:question) { create(:question) }
+
+  describe 'Authenticated user' do
+    background do
+      sign_in(user)
+      visit question_path(question)
+    end
+
+    fscenario 'give comment for question', js: true do
+      within '.question' do
+        # save_and_open_page
+        click_on 'Add comment'
+        fill_in 'Comment body', with: 'My comment'
+        click_on 'Save comment'
+
+        expect(page).to_not have_selector "#Add-Question-Comment-#{question.id}"
+      end
+
+      within '.question-comments' do
+        expect(page).to have_content 'My comment'
+      end
+    end
+
+    context 'multiple session' do
+
+    end
+
+    scenario 'give comment with error', js: true do
+
+    end
+  end
+
+  scenario 'Unauthenticated user tries set comment' do
+
+  end
+end
