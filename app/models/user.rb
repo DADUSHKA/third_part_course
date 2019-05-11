43a52de76a53956a07d14b61096d2248061d9a79
Victor Guard <vicstor@bk.ru>
2019-05-11 15:51:43 +0300

    intermediate commit with created model authorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,15 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable,
+          :omniauthable, omniauth_providers: [:github]
 
   has_many :questions, foreign_key: 'author_id', dependent: :destroy
   has_many :answers, foreign_key: 'author_id', dependent: :destroy
   has_many :awards
   has_many :votes, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :authorizations, dependent: :destroy
 
   def author_of?(obj)
     obj.author_id == id
@@ -16,5 +18,16 @@ class User < ApplicationRecord
 
   def voted?(resource)
     votes.exists?(voteable: resource)
+  end
+
+  def self.find_for_oauth(auth)
+    authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
+    return authorization.user if authorization
+
+    email = auth.info[:email]
+    user = User.where(email: email).first
+
+    user.authorizations.create(provider: auth.provider, uid: auth.uid) if user
+    user
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe User, type: :model do
   it { should have_many(:awards) }
   it { should have_many(:votes) }
   it { should have_many(:comments) }
+  it { should have_many(:authorizations).dependent(:destroy) }
 
   describe "Author_of?" do
     let!(:user) { create(:user) }
@@ -36,6 +37,67 @@ RSpec.describe User, type: :model do
      it "user is not author of the vote" do
       votes << vote
       expect(not_voted_user).to_not be_voted(answer)
+    end
+  end
+
+  describe '.find_for_oauth' do
+    let!(:user) { create(:user) }
+    let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '123456') }
+
+    context "user already has authorization" do
+      it "return the user" do
+        user.authorizations.create(provider: 'facebook', uid: '123456')
+        expect(User.find_for_oauth(auth)).to eq user
+      end
+    end
+
+    context "user has not authorization" do
+      context "user already exists" do
+        let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '123456', info: { email: user.email }) }
+        it 'does not create new user' do
+          expect{ User.find_for_oauth(auth) }.to_not change(User, :count)
+        end
+
+        it 'creates authorization for user' do
+          expect{ User.find_for_oauth(auth) }.to change(user.authorizations, :count).by(1)
+        end
+
+        it 'creates authorization with provider and uid' do
+          user = User.find_for_oauth(auth)
+          authorization = user.authorizations.first
+
+          expect(authorization.provider).to eq auth.provider
+          expect(authorization.uid).to eq auth.uid
+        end
+
+        it 'return user' do
+          expect(User.find_for_oauth(auth)).to eq user
+        end
+      end
+
+      context 'user has not exist' do
+        let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '123456', info: { email: 'new@user.com' }) }
+
+        it 'create new user' do
+
+        end
+
+        it 'return user' do
+
+        end
+
+        it 'fill user email' do
+
+        end
+
+        it 'create authorization for user' do
+
+        end
+
+        it 'create authorization with provider and uid' do
+
+        end
+      end
     end
   end
 end
