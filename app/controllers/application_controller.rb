5fe2d71476223e43245db2a013b0c37337fef03a
Victor Guard <vicstor@bk.ru>
2019-05-14 16:54:29 +0300

    handler: for js and json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
 
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, alert: exception.message
+    respond_to do |format|
+      format.html { redirect_to root_path, alert: exception.message }
+      format.json { render json: exception.message, status: :forbidden }
+      format.js { render json: exception.message, status: :forbidden }
+    end
   end
 
   check_authorization unless: :devise_controller?
