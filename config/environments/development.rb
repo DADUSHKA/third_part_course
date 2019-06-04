06c1ff8d59fd12eaa316493d56ab5450db302ddc
Victor Guard <vicstor@bk.ru>
2019-06-04 14:11:28 +0300

    creati caches

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,8 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
+  # config.action_controller.perform_caching = true
+  config.action_controller.enable_fragment_cache_logging = true
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
