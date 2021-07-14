72f8c5bf32ecc82f37525a98f4cd699559935472
Victor Storozhev <victor.storozhev@jetruby.com>
2021-07-14 15:11:48 +0300

    Проблема с master.key решена, изменены настройки capistrano.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "193.111.63.118", user: "vicstor", roles: %w{app db web}, primary: true
+server "80.78.251.147", user: "server", roles: %w{app db web}, primary: true
 set :rail_env, :production
 
 # Custom SSH Options
